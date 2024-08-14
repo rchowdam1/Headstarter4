536c6e2314c50e85cfc2c0bbc68204545073b56d
MahdMalik <mahdmahdmalik@gmail.com>
2024-08-14 17:27:52 -0500

    push

--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ import {useState, useRef, useEffect} from 'react'
 
 export default function Home()
 {
-    return
-    (
-        
-    )
+    return  <Box sx={{my: 6}}>
+    <Typography variant="h4" component="h2" gutterBottom>Features</Typography>
+    <Grid container spacing={4}>
+        {/* Feature items */}
+    </Grid>
+    </Box>
 }
