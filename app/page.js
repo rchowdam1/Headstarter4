8ec97f4ac17d0b9903172ccb316ea667acc0382c
MahdMalik <mahdmahdmalik@gmail.com>
2024-08-15 17:32:14 -0500

    push

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 import getStripe from '@/utils/get-stripe'
-import { Typography } from '@mui/material'
-import { UserButton } from '@clerk/nextjs'
+import { Typography, Container, AppBar, Toolbar, Button, Box, Grid } from '@mui/material'
+import { UserButton, SignedIn, SignedOut } from '@clerk/nextjs'
+import Head from 'next/head'
 
 export default function Home()
 {
-  return
-  (
-    <Container maxWidth="1g">
+  return (
+    <Container maxWidth="lg">
       <Head>
-        <title>Flashcard Saas</title>
+        <title>Flashcard SaaS</title>
         <meta name = "description" content = "Create flashcard from your text"/>
       </Head>
 
@@ -17,7 +17,6 @@ export default function Home()
         <Toolbar>
           <Typography variant="h6">Flashcard Saas</Typography>
           <SignedOut>
-            <Button>Login</Button>
             <Button>Sign Up</Button>
           </SignedOut>
           <SignedIn>
@@ -25,6 +24,22 @@ export default function Home()
           </SignedIn>
         </Toolbar>
       </AppBar>
+
+      <Box sx={{textAlign: "center", my: 4}}>
+        <Typography variant="h2">Welcome to flashcard SaaS</Typography>
+        <Typography variant="h5"> {' '}The easiest way to maek flashcards from your text</Typography>
+        <Button variant="contained" color="primary" sx={{mt: 2}}>Get Started</Button>
+      </Box>
+
+      <Box sx={{my:6}}>
+        <Typography variant="h4" components="h2">Features</Typography>
+        <Grid contained spacing={4}>
+          <Grid item xs = {12} md = {4}>
+            <Typography variant="h6">Easy Text Input</Typography>
+            <Typography>{' '}Simply input your text, our software does the rest! Now it's much easier to make flashcards!</Typography>
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   )
 }
