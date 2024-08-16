df78afba3c7180a8582f75614d69bd99a088faa6
MahdMalik <mahdmahdmalik@gmail.com>
2024-08-15 19:29:58 -0500

    push

--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ export default function Home()
 
       <Box sx={{textAlign: "center", my: 4}}>
         <Typography variant="h2">Welcome to flashcard SaaS</Typography>
-        <Typography variant="h5"> {' '}The easiest way to maek flashcards from your text</Typography>
+        <Typography variant="h5"> {' '}The easiest way to make flashcards from your text</Typography>
         <Button variant="contained" color="primary" sx={{mt: 2}}>Get Started</Button>
       </Box>
 
@@ -52,19 +52,29 @@ export default function Home()
         <Typography variant="h4">Pricing</Typography> 
         <Grid container spacing={4}>
           <Grid item xs = {12} md = {4}>
-            <Box sx={{p: 3, border: "1px solid", borderColor: "grey.300", borderRadius: 2}}></Box>
-            <Typography variant="h6">Pricing Header here</Typography>
-            <Typography>Pricing Desc here</Typography>
+            <Box sx={{p: 3, border: "1px solid", borderColor: "grey.300", borderRadius: 2}}>
+            <Typography variant="h5">Basic</Typography>
+            <Typography variant="h6">Free</Typography>
+            <Typography>{' '}Access to basic flashcard features and limited storage</Typography>
+            <Button variant="contained" color="primary">Choose Basic</Button>
+            </Box>
           </Grid>
           <Grid item xs = {12} md = {4}>
-            <Box sx={{p: 3, border: "1px solid", borderColor: "grey.300", borderRadius: 2}}></Box>
-            <Typography variant="h6">Pricing Header here</Typography>
-            <Typography>Pricing Desc here</Typography>
+            <Box sx={{p: 3, border: "1px solid", borderColor: "grey.300", borderRadius: 2}}>
+            <Typography variant="h5">Pro</Typography>
+            <Typography variant="h6">$5 Per Month</Typography>
+            <Typography>{' '}Access to advanced flashcard features and greater storage</Typography>
+            <Button variant="contained" color="primary">Choose Pro</Button>
+            </Box>
           </Grid>
           <Grid item xs = {12} md = {4}>
-            <Box sx={{p: 3, border: "1px solid", borderColor: "grey.300", borderRadius: 2}}></Box>
-            <Typography variant="h6">Pricing Header here</Typography>
-            <Typography>Pricing Desc here</Typography>
+            <Box sx={{p: 3, border: "1px solid", borderColor: "grey.300", borderRadius: 2}}>
+            <Typography variant="h5">Ultra</Typography>
+            <Typography variant="h6">$10 Per Month</Typography>
+            <Typography>{' '}Access to additional flashcard features and unlimited storage</Typography>
+            <Button variant="contained" color="primary">Choose Ultra</Button>
+            </Box>
+
           </Grid>
         </Grid>          
       </Box>
