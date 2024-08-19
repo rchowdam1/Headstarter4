471ee28eb331d0c555b5690ab413ede9981af070
MahdMalik <mahdmahdmalik@gmail.com>
2024-08-19 16:14:29 -0500

    push

--- app/page.js
+++ app/page.js
@@ -58,15 +58,15 @@ export default function Home() {
         <Grid container spacing={4}>
           <Grid item xs={12} md={4}>
             <Typography variant="h6">Easy Text Input</Typography>
-            <Typography>{' '}Simply input your text, our software does the rest! Now it's much easier to make flashcards!</Typography>
+            <Typography>{' '}Simply input your text, our software does the rest! Now it is much easier to make flashcards!</Typography>
           </Grid>
           <Grid item xs={12} md={4}>
             <Typography variant="h6">Easy Text Input</Typography>
-            <Typography>{' '}Simply input your text, our software does the rest! Now it's much easier to make flashcards!</Typography>
+            <Typography>{' '}Simply input your text, our software does the rest! Now it is much easier to make flashcards!</Typography>
           </Grid>
           <Grid item xs={12} md={4}>
             <Typography variant="h6">Easy Text Input</Typography>
-            <Typography>{' '}Simply input your text, our software does the rest! Now it's much easier to make flashcards!</Typography>
+            <Typography>{' '}Simply input your text, our software does the rest! Now it is much easier to make flashcards!</Typography>
           </Grid>
         </Grid>
       </Box>
